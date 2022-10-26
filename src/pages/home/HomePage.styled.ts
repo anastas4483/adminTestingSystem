eaa282b4b17af8665690013d165e366a1cb3b956
Sorokina <nastasya.sorokina1@gmail.com>
2022-10-26 14:43:20 +0300

    add content to HomePAge

--- src/pages/home/HomePage.styled.ts
+++ src/pages/home/HomePage.styled.ts
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+padding-top: 50px;
+
+`;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+import { Wrapper } from './HomePage.styled'
 
-export const HomePage: FC = () => {
-    return (
-        <div>home page</div>
-    )
-}
+export const HomePage: FC = () => (
+    <Wrapper>
+        This is home page, you can go to &nbsp; <Link to={'tests'}> Tests</Link> &nbsp; or &nbsp;<Link to={'users'}> Users</Link>
+    </Wrapper>
+)
